0359c2ad6ef53b8780f3a48a1e520d428aa662af
Chris <cjmarsh725@gmail.com>
2019-08-05 13:08:11 -0700

    Axios testing

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-const moment = require('moment');
-
-console.log(moment().startOf('year').add(10, 'M').format('M/D/YY'));
+const axios = require("axios");
+axios.get("https://api.guildwars2.com/v2/professions").then(response => {
+  const professionList = response.data;
+  const requests = [];
+  professionList.forEach(profession => {
+    requests.push(axios.get(`https://api.guildwars2.com/v2/professions/${profession}`));
+  });
+  const professions = {};
+  axios.all(requests).then(response => {
+    professionList.forEach(profession => {
+      professions[profession] = response.map(res => res.data).find(data => data.id === profession);
+    });
+    this.setState({ professionList, professions });
+  }).catch(error => console.error("Individual professions endpoint: " + error));
+}).catch(error => console.error("Profession list endpoint: " + error));
